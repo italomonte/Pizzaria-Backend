a0e5242b20f0ef5188e1e82ee20da43dd9b0fd94
Ítalo Monte <italopessoal@outlook.com>
2025-01-08 20:42:50 -0400

    feat: detailUser controller & service

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,18 @@
 import {Router} from 'express'
+
+// USER
 import {CreateUserController} from "./controllers/user/CreateUserController"
 import {AuthUserController} from "./controllers/user/AuthUserController"
 import {DetailUserController} from "./controllers/user/DetailUserController"
+
+// CATEGORY 
+import {CreateCategoryController} from "./controllers/category/CreateCategoryController"
+import {ListCategoryController} from "./controllers/category/ListCategoryController"
+
+//PRODUCT
+import { CreateProductController } from './controllers/product/CreateProductController'
+
+// MIDDLEWARES
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
@@ -12,5 +23,15 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+ 
+//-- ROTAS CATEGORY 
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, new CreateProductController().handle)
+
 
 export { router };
