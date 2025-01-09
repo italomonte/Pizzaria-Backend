4c6142aa98fc69d412eb7e8cb763f1df986b5a16
Ítalo Monte <italopessoal@outlook.com>
2025-01-08 20:42:14 -0400

    feat: middleware isAuthenticated

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,6 +1,42 @@
+import { Payload } from "@prisma/client/runtime/library";
 import { Response, Request, NextFunction } from "express";
+import { verify } from "jsonwebtoken"
 
-export function isAuthenticated(req:Request, res:Response, next: NextFunction) {
-    console.log("Chamou o middleware")
-    next()
+interface PayLoad {
+    sub: string;
+}
+
+export function isAuthenticated
+    (req:Request,
+     res:Response
+     , next: NextFunction
+    ) {
+
+    // Receber o token
+    const authToken = req.headers.authorization;
+
+    //Verificar se existe um token
+    if (!authToken) {
+        return res.status(401).end()
+    }
+
+    const [, token] = authToken.split(" ")
+
+    console.log(token)
+
+    try {
+        // validar token
+        const { sub } = verify(
+        token,
+        process.env.JWT_SECRET
+        ) as PayLoad
+
+        // adicionando numa variavel da requisição o id do usuário que está acessando a rota
+        req.user_id = sub;
+
+        return next()
+
+    } catch (err) {
+        return res.status(401).end()
+    }
 }
