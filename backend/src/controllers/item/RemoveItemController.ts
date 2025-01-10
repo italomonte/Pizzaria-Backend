52645078d20bb738af8475f0f96c2a79ef46aa74
Ítalo Monte <italopessoal@outlook.com>
2025-01-09 23:32:58 -0400

    feat: remove item order

---
+++ backend/src/controllers/item/RemoveItemController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { RemoveItemService } from "../../services/item/RemoveItemService";
+
+class RemoveItemController {
+    async handle(req: Request, res: Response) {
+
+        const item_id = req.query.item_id as string
+
+        const removeItemService = new RemoveItemService()
+
+        const item = await removeItemService.execute({item_id})
+
+        res.json(item)
+    }
+}
+
+export {RemoveItemController}
